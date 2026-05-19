d80a5fcc158459c724d307175b2b625de062f689
chahinezou2005-ops <chahinez.ou2005@gmail.com>
2026-05-19 20:12:30 +0100

    modification du chahinez patient

--- APP/controllers/PatientController/AccueilController.php
+++ APP/controllers/PatientController/AccueilController.php
@@ -6,7 +6,7 @@ require_once ROOT . '/APP/models/Pmodel/PatientModel.php';
 
 // --- PARTIE STATISTIQUES (Point 3) ---
 // On récupère la connexion PDO globale
-global $pdo; 
+global $pdo;
 
 // --- INSTANCIATION DE LA CLASSE ---
 $patientModel = new PatientModel($pdo);
@@ -23,10 +23,12 @@ $specialites = $patientModel->getAllSpecialites();
 // 2. On vérifie si l'utilisateur a cliqué sur une spécialité (via l'URL ?spec=ID)
 $medecins = [];
 $specSelectionnee = $_GET['spec'] ?? null;
+$recherche = $_GET['search'] ?? '';
 
 if ($specSelectionnee) {
-    // Utilisation de la méthode via l'objet
     $medecins = $patientModel->getMedecinsBySpec($specSelectionnee);
+} elseif ($recherche) {
+    $medecins = $patientModel->rechercherMedecins($recherche);
 }
 
 // 2. On charge la vue Accueil.php
